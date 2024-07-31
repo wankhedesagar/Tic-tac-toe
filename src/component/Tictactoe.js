110125fa893f8ecd7aa09c22cbf675badd12ded6
sagar wankhede <swankhede639@gmail.com>
2024-07-31 18:19:22 +0530

    add one more state for getting winner of this game and add checkWinner function for direction check to find winner inside checkWinner function adding a loop and add inside handleClick function newBoard vaiable for store all value

--- src/component/Tictactoe.js
+++ src/component/Tictactoe.js
@@ -3,18 +3,42 @@ import React,{useState} from 'react'
 function Tictactoe() {
     const [board, setBoard] = useState(Array(9).fill(null));
     const [isXTurn, setXTurn] = useState(true);
+    const [winner, setWinner] = useState(null);
 
 
-
+    const checkWinner = (board) => {
+        const combination = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        for (let i = 0; i < combination.length; i++) {
+          const [a, b, c] = combination[i];
+          if (board[a] === board[b] && board[b] === board[c]) {
+            return combination[i];
+          }
+        }
+        return null;
+      };
 
     const handleClick = (index) => {
-     
+        if (board[index] !== null) {
+          return;
+        }
         console.log(index, "clicked");
         const newBoard = [...board];
         newBoard[index] = isXTurn ? "X" : "O";
         setBoard(newBoard);
         setXTurn(!isXTurn);
-       
+        const winnerCombination = checkWinner(newBoard);
+        if (winnerCombination) {
+          setWinner(newBoard[winnerCombination[0]]);
+        }
       };
 
     const renderSquare = (index) => {
@@ -22,8 +46,17 @@ function Tictactoe() {
             <button onClick={() => handleClick(index)} className='square'>{board[index]}</button>
         )
     }
+
+
+
+    
   return (
-   
+    <React.Fragment>
+      <h1 className="heading">Tic-Tac-Toe</h1>
+      <div className="msg-container"> 
+      {winner && `${winner} is winner of this Game.`}
+      </div>
+
     <div className="board">
     <div className="board-row">
       {renderSquare(0)}
@@ -41,7 +74,9 @@ function Tictactoe() {
       {renderSquare(8)}
     </div>
   </div>
+  </React.Fragment>
   )
 }
 
-export default Tictactoe
+
+export default Tictactoe;
